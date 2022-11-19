ce66ad71be8e43c2f35a01a4a5d64d40dc8ae397
flyingpig233-cpu <1686079252@qq.com>
2022-11-19 15:00:39 +0800

    github

--- example.c
+++ example.c
@@ -5,27 +5,25 @@ int main(void)
     FILE* read_fp;
     char buffer[BUFSIZ + 1] = { 0 };
     int chars_read;
-	printf("Enter the file to build: ");
-	fflush(0);
-	char filename[201];
-	if (scanf("%200s", filename) != 1) {
-		return EXIT_FAILURE;
-	}
-	char command[301];
-	sprintf(command, "gcc %s 2>&1", filename);
+    printf("Enter the file to build: ");
+    fflush(0);
+    char filename[201];
+    if (scanf("%200s", filename) != 1) {
+        return EXIT_FAILURE;
+    }
+    char command[301];
+    sprintf(command, "gcc %s 2>&1", filename);
     printf("Running command: %s\n", command);
     read_fp = popen(command, "r");
     if (read_fp != NULL) {
         chars_read = fread(buffer, sizeof(char), BUFSIZ, read_fp);
         if (chars_read > 0) {
             struct output_list* list = parse_cc_output(buffer);
-			drop_output_list(list);
-			printf("Success to drop output lists\n");
-
+            drop_output_list(list);
+            printf("Success to drop output lists\n");
         }
         pclose(read_fp);
         return 0;
     }
     return -1;
 }
-
